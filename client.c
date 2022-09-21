c28add4d1539d4bb4fea342a6fee3f26074fd1a4
Nizar Hamdan <nhamdan@c4r1p3.42nice.fr>
2022-09-21 16:45:24 +0200

    v2 non definitif

--- client.c
+++ client.c
@@ -6,70 +6,50 @@
 /*   By: nhamdan <nhamdan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:12:14 by nhamdan           #+#    #+#             */
-/*   Updated: 2022/09/18 23:19:41 by nhamdan          ###   ########.fr       */
+/*   Updated: 2022/09/21 16:42:23 by nhamdan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include "libft/libft.h"
 
-void	send_to_server(pid_t pid, char *str)
+static void		read_signal_client(int sig_id)
 {
-	int		i;
-	char	c;
+		(void) sig_id;
+}
+
+static void		send_char(pid_t pid, unsigned char c)
+{
+	int	i;
 
 	i = 8;
-	while (*str)
-	{
-		c = *str++;
-		while (i--)
+
+	while(i--)
 		{
 			if (c >> i & 1)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
+			pause();
 			usleep(100);
 		}
-		while (i <= 8)
-		{
-			kill(pid, SIGUSR2);
-			usleep(100);
-			i++;
-		}
-	}
 }
 
-int	ft_minitalk_error(int argc, char *argv)
+static void		send_str(pid_t pid, char *str)
 {
-	int	i;
-
-	i = 0;
-	if (argc != 3)
-	{
-		ft_putstr_fd("Besoin de 2 args : PID & str\n", 1);
-		return (0);
-	}
-	while (argv[i])
-	{
-		if (!ft_isdigit(argv[i]))
+	while(*str)
 		{
-			ft_putstr_fd("Le PID doit etre un nombre", 1);
-			return (0);
+			send_char(pid, (unsigned char) *str);
+			str++;
 		}
-		i++;
-	}
-	return (1);
+	send_char(pid, '\n');
 }
 
-int	main(int argc, char **argv)
-{
-	int pids;
-	int i;
-
-	i = 0;
-	if (ft_minitalk_error(argc, argv[1]) == 0)
-		return (1);
-	pids = ft_atoi(argv[1]);
-	send_to_server(pids, argv[2]);
-	return (0);
+int main(int argc, char *argv[])
+{	
+	pid_t 	pid;
+	pid = ft_atoi(argv[1]);
+	signal(SIGUSR1, read_signal_client);
+	send_str(pid, argv[2]);
+	return(0);
 }
--- server.c
+++ server.c
@@ -6,28 +6,32 @@
 /*   By: nhamdan <nhamdan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:12:29 by nhamdan           #+#    #+#             */
-/*   Updated: 2022/09/19 19:13:53 by nhamdan          ###   ########.fr       */
+/*   Updated: 2022/09/21 16:44:05 by nhamdan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include "libft/libft.h"
 
-static void	sig_handler(int sig)
+static void	read_signal(int sig_id, siginfo_t *info, void *context)
 {
-	static int	i;
-	static unsigned char	c;
+	static unsigned char		c;
+	static int		i;
 
-	i = 0;
-	c |= sig == SIGUSR1;
-	if (++i == 8)
-	{
-		i = 0;
-		ft_putchar_fd(c, 1);
-		c = 0;
-	}
-	else
-		c <<= 1;
+
+	(void) context;
+	i += 1;
+	c |= (sig_id == SIGUSR1);
+		if(i < 8)
+			c <<= 1;
+		if(i == 8)
+		{
+			write(1, &c,1);
+			c = 0;
+			i = 0;
+		}
+	usleep(100);
+	kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
@@ -38,10 +42,10 @@ int	main(void)
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
 	str.sa_flags = SA_SIGINFO;
-	str.sa_handler = sig_handler;
+	str.sa_sigaction = read_signal;
 	sigaction(SIGUSR1, &str, NULL);
 	sigaction(SIGUSR2, &str, NULL);
 	while (1)
 		pause();
-	return (0);
+	return (0);	
 }
